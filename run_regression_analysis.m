5aed22ff5fa71ec1bbc74fd939f24165dbf9c327
Matthias Treder <matthias.treder@gmail.com>
2020-10-09 10:52:40 +0200

    bugfixes

--- run_regression_analysis.m
+++ run_regression_analysis.m
@@ -23,7 +23,7 @@ n_constraints = 5;
 
 % iterations x constraints (unconstrained,zero,bounded 0.1, 0.2, 0.3) x models (linear,ridge,kernelridge) x train/inference phase
 times = zeros(n_iterations, n_constraints, 3, 2);
-mse = zeros(n_iterations, n_constraints, 3, 2);
+mae = zeros(n_iterations, n_constraints, 3, 2);
 corrs = zeros(n_iterations, n_constraints, 3, 2);
 
 fprintf('Starting regression loop with %d iterations\n', n_iterations)
@@ -40,63 +40,68 @@ for n = 1:n_iterations
     
     for m = 1:n_constraints
     
-        if m==0 % Standard Scikit-Learn models
+        if m==1 % Standard Scikit-Learn models
             param_linreg.correlation_bound = [];
             param_ridge.correlation_bound = [];
             param_kernel_ridge.correlation_bound = [];
             
-        elseif m==1 % zero correlation
-            linreg = ccr.LinearRegression(correlation_bound=0)
-            ridge = GridSearchCV(ccr.Ridge(correlation_bound=0), param_grid=tune_Ridge, scoring='neg_mean_squared_error')
-            kr = GridSearchCV(ccr.KernelRidge(correlation_bound=0), param_grid=tune_KernelRidge, scoring='neg_mean_squared_error')
+        elseif m==2 % zero correlation
+            param_linreg.correlation_bound = 0;
+            param_ridge.correlation_bound = 0;
+            param_kernel_ridge.correlation_bound = 0;
 
-        elseif m==2 % bounded correlation 0.1
-            linreg = ccr.LinearRegression(correlation_bound=0.1)
-            ridge = GridSearchCV(ccr.Ridge(correlation_bound=0.1), param_grid=tune_Ridge, scoring='neg_mean_squared_error')
-            kr = GridSearchCV(ccr.KernelRidge(correlation_bound=0.1), param_grid=tune_KernelRidge, scoring='neg_mean_squared_error')
+        elseif m==3 % bounded correlation 0.1
+            param_linreg.correlation_bound = 0.1;
+            param_ridge.correlation_bound = 0.1;
+            param_kernel_ridge.correlation_bound = 0.1;
 
-        elseif m==3 % bounded correlation 0.2
-            linreg = ccr.LinearRegression(correlation_bound=0.2)
-            ridge = GridSearchCV(ccr.Ridge(correlation_bound=0.2), param_grid=tune_Ridge, scoring='neg_mean_squared_error')
-            kr = GridSearchCV(ccr.KernelRidge(correlation_bound=0.2), param_grid=tune_KernelRidge, scoring='neg_mean_squared_error')
+        elseif m==4 % bounded correlation 0.2
+            param_linreg.correlation_bound = 0.2;
+            param_ridge.correlation_bound = 0.2;
+            param_kernel_ridge.correlation_bound = 0.2;
 
-        elseif m==4 % bounded correlation 0.3
-            linreg = ccr.LinearRegression(correlation_bound=0.3)
-            ridge = GridSearchCV(ccr.Ridge(correlation_bound=0.3), param_grid=tune_Ridge, scoring='neg_mean_squared_error')
-            kr = GridSearchCV(ccr.KernelRidge(correlation_bound=0.3), param_grid=tune_KernelRidge, scoring='neg_mean_squared_error')
+        elseif m==5 % bounded correlation 0.3
+            param_linreg.correlation_bound = 0.3;
+            param_ridge.correlation_bound = 0.3;
+            param_kernel_ridge.correlation_bound = 0.3;
         end
         
         % Fit models
-        mse(n,m,0,0), mse(n,m,0,1), times(n,m,0,0), times(n,m,0,1), corrs(n,m,0,0), corrs(n,m,0,1) = fit_model(linreg, X_train, y_train, X_test, y_test);
-        mse(n,m,1,0), mse(n,m,1,1), times(n,m,1,0), times(n,m,1,1), corrs(n,m,1,0), corrs(n,m,1,1) = fit_model(ridge, X_train, y_train, X_test, y_test);
-        mse(n,m,2,0), mse(n,m,2,1), times(n,m,2,0), times(n,m,2,1), corrs(n,m,2,0), corrs(n,m,2,1) = fit_model(kr, X_train, y_train, X_test, y_test);
+        [mae(n,m,1,1), mae(n,m,1,2), times(n,m,1,1), times(n,m,1,2), corrs(n,m,1,1), corrs(n,m,1,2)] = fit_model(@train_ridge, param_linreg, @test_ridge, X_train, y_train, X_test, y_test);
+        [mae(n,m,2,1), mae(n,m,2,2), times(n,m,2,1), times(n,m,2,2), corrs(n,m,2,1), corrs(n,m,2,2)] = fit_model(@train_ridge, param_ridge, @test_ridge, X_train, y_train, X_test, y_test);
+        [mae(n,m,3,1), mae(n,m,3,2), times(n,m,3,1), times(n,m,3,2), corrs(n,m,3,1), corrs(n,m,3,2)] = fit_model(@train_kernel_ridge, param_kernel_ridge, @test_kernel_ridge, X_train, y_train, X_test, y_test);
     end
 end
 
 
-save('regression_results_pac2019.mat', times, mse, corrs);
+save('regression_results_pac2019.mat', times, mae, corrs);
 
 
 
-    function  [mae_train, mae_test, train_time, test_time, corr_train, corr_test] = fit_model(train_fun, test_fun, X_train, y_train, X_test, y_test):
-        % Fits model, calculates MSE on train and test data and performs timing.
+    function  [mae_train, mae_test, train_time, test_time, corr_train, corr_test] = fit_model(train_fun, param, test_fun, X_train, y_train, X_test, y_test)
+        % Fits model, calculates MAE on train and test data and performs timing.
         % train
         tic
-        model = train_fun(X_train, y_train);
+        model = train_fun(param, X_train, y_train);
         train_time = toc;
 
         % predict
         tic
-        yhat_train = test_fun(model, X_train)
+        yhat_train = test_fun(model, X_train);
         yhat_test  = test_fun(model, X_test);
         test_time = toc;
 
         % target-residual correlation
-        corr_train = corr(y_train, y_train-yhat_train)
-        corr_test = corr(y_test, y_test-yhat_test)
+        corr_train = corr(y_train, y_train-yhat_train);
+        corr_test = corr(y_test, y_test-yhat_test);
+    
+        % error estimates
+        mae_train = mean_absolute_error(y_train, yhat_train);
+        mae_test =  mean_absolute_error(y_test, yhat_test);
+    end
 
-        return mean_absolute_error(y_train, yhat_train), mean_absolute_error(y_test, yhat_test), \
-                train_time, test_time, corr_train, corr_test
+    function mae = mean_absolute_error(y, yhat)
+        mae = mean(abs(y-yhat));
     end
     
 end
