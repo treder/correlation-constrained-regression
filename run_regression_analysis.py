d1417dd4612818286eed47dbed3cadd0bb23f28a
Matthias Treder <matthias.treder@gmail.com>
2020-11-23 13:49:18 +0000

    added approach 1 and 2

--- run_regression_analysis.py
+++ run_regression_analysis.py
@@ -11,47 +11,7 @@ from sklearn.model_selection import train_test_split, GridSearchCV
 sys.path.append('/home/matthias/mt03/python_tools')
 import file_tools as ft
 import correlation_constrained_regression as ccr
-
-def load_data(dataset):
-    '''
-    Loads a regression dataset
-
-    Returns:
-        X, y - features and targets
-    '''
-    print('Loading data')
-    if dataset == 'pac2019':
-        df = pd.read_csv(datadir + 'PAC2019_BrainAge_ICA.csv')
-
-        # extract features and age
-        feature_ix = [ix for ix, name in enumerate(df.columns) if name.startswith('loadings')]
-        X = df.iloc[:, feature_ix].to_numpy()
-        y = df['age'].to_numpy()
-
-    print(X.shape, y.shape)
-    return X, y
-
-def fit_model(model, X_train, y_train, X_test, y_test):
-    '''
-    Fits model, calculates MSE on train and test data and performs timing.
-    '''
-    # train
-    start_time = time.time()
-    model.fit(X_train, y_train)
-    train_time = time.time() - start_time
-    
-    # predict
-    start_time = time.time()
-    yhat_train = model.predict(X_train)
-    yhat_test  = model.predict(X_test)
-    test_time = time.time() - start_time
-
-    # target-residual correlation
-    corr_train = np.corrcoef(y_train, y_train-yhat_train)[0,1]
-    corr_test = np.corrcoef(y_test, y_test-yhat_test)[0,1]
-    
-    return mean_absolute_error(y_train, yhat_train), mean_absolute_error(y_test, yhat_test), \
-            train_time, test_time, corr_train, corr_test
+import analysis_tools as at
 
 dataset = 'pac2019'
 X, y = load_data(dataset)
@@ -65,12 +25,48 @@ tune_Ridge = {'alpha': [1e-3, 1e-2, 1e-1, 1, 10]}
 
 import time
 n_iterations = 100
-n_constraints = 5
+n_constraints = 7
 
 # iterations x constraints (unconstrained,zero,bounded 0.1, 0.2, 0.3) x models (linear,ridge,kernelridge) x train/inference phase
 times = np.zeros((n_iterations, n_constraints, 3, 2))
-mse = np.zeros((n_iterations, n_constraints, 3, 2))
+mae = np.zeros((n_iterations, n_constraints, 3, 2))
 corrs = np.zeros((n_iterations, n_constraints, 3, 2))
+
+def fit_model_and_correct(model, X_train, y_train, X_test, y_test, approach):
+    '''
+    Fits model, calculates MAE on train and test data and performs timing.
+    '''
+    # Step 1: Standard regression
+    model.fit(X_train, y_train)
+    yhat_train = model.predict(X_train)
+    yhat_test  = model.predict(X_test)
+    
+    # Step 2: Train correction coefficients on train data
+    delta_train = yhat_train - y_train
+    if approach == 1:
+        # regress delta on y
+        fit = sm.OLS(delta_train, sm.add_constant(y_train)).fit()
+        b0, b1 = fit.params
+        delta_train = delta_train - b1*y_train - b0
+        delta_test = delta_test - b1*y_test - b0
+        mae_train = (np.abs(delta_train)).mean()
+        mae_test = (np.abs(delta_test)).mean()
+    else:
+        # regress yhat on y
+        fit = sm.OLS(yhat_train, sm.add_constant(y_train)).fit()
+        b0, b1 = fit.params
+        yhat_train = (yhat_train - b0) / b1
+        yhat_test = (yhat_test - b0) / b1
+        delta_train = yhat_train - y_train
+        delta_test = yhat_test - y_test
+        mae_train = mean_absolute_error(y_train, yhat_train)
+        mae_test = mean_absolute_error(y_test, yhat_test)
+        
+
+    # target-residual correlation
+    corr_train = np.corrcoef(y_train, -delta_train)[0,1]
+    corr_test = np.corrcoef(y_test, -delta_test)[0,1]
+    return mae_train, mae_test, corr_train, corr_test
 
 print(f'Starting regression loop with {n_iterations} iterations')
 for n in range(n_iterations):
@@ -105,12 +101,21 @@ for n in range(n_iterations):
             linreg = ccr.LinearRegression(correlation_bound=0.3)
             ridge = GridSearchCV(ccr.Ridge(correlation_bound=0.3), param_grid=tune_Ridge, scoring='neg_mean_squared_error')
             kr = GridSearchCV(ccr.KernelRidge(correlation_bound=0.3), param_grid=tune_KernelRidge, scoring='neg_mean_squared_error')
+            
+        elif m >= 5: # Standard approach + Post hoc correction
+            linreg = LinearRegression()
+            ridge = GridSearchCV(Ridge(), param_grid=tune_Ridge, scoring='neg_mean_squared_error')
+            kr = GridSearchCV(KernelRidge(), param_grid=tune_KernelRidge, scoring='neg_mean_squared_error')
+            approach = m - 4
+            mae[n, m, 0, 0], mae[n, m, 0, 1], corrs[n, m, 0, 0], corrs[n, m, 0, 1] = fit_model_and_correct(linreg, X_train, y_train, X_test, y_test, approach)
+            mae[n, m, 1, 0], mae[n, m, 2, 1], corrs[n, m, 1, 0], corrs[n, m, 1, 1] = fit_model_and_correct(ridge, X_train, y_train, X_test, y_test, approach)
+            mae[n, m, 2, 0], mae[n, m, 1, 1], corrs[n, m, 2, 0], corrs[n, m, 2, 1] = fit_model_and_correct(kr, X_train, y_train, X_test, y_test, approach)
 
         # Fit models
-        mse[n, m, 0, 0], mse[n, m, 0, 1], times[n, m, 0, 0], times[n, m, 0, 1], corrs[n, m, 0, 0], corrs[n, m, 0, 1] = fit_model(linreg, X_train, y_train, X_test, y_test)
-        mse[n, m, 1, 0], mse[n, m, 1, 1], times[n, m, 1, 0], times[n, m, 1, 1], corrs[n, m, 1, 0], corrs[n, m, 1, 1] = fit_model(ridge, X_train, y_train, X_test, y_test)
-        mse[n, m, 2, 0], mse[n, m, 2, 1], times[n, m, 2, 0], times[n, m, 2, 1], corrs[n, m, 2, 0], corrs[n, m, 2, 1] = fit_model(kr, X_train, y_train, X_test, y_test)
+        mae[n, m, 0, 0], mae[n, m, 0, 1], times[n, m, 0, 0], times[n, m, 0, 1], corrs[n, m, 0, 0], corrs[n, m, 0, 1] = fit_model(linreg, X_train, y_train, X_test, y_test)
+        mae[n, m, 1, 0], mae[n, m, 1, 1], times[n, m, 1, 0], times[n, m, 1, 1], corrs[n, m, 1, 0], corrs[n, m, 1, 1] = fit_model(ridge, X_train, y_train, X_test, y_test)
+        mae[n, m, 2, 0], mae[n, m, 2, 1], times[n, m, 2, 0], times[n, m, 2, 1], corrs[n, m, 2, 0], corrs[n, m, 2, 1] = fit_model(kr, X_train, y_train, X_test, y_test)
 
 # save results
-pickle.dump( (times, mse, corrs), open(f'regression_results_{dataset}.pickle', 'wb' ) )
+pickle.dump( (times, mae, corrs), open(f'regression_results_{dataset}.pickle', 'wb' ) )
 
