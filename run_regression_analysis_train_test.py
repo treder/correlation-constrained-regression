65dd770be0d63986dc7fca49aa8a6b6f73118330
Matthias Treder <matthias.treder@gmail.com>
2020-12-09 21:55:40 +0000

    measure time for approach 1/2

--- run_regression_analysis_train_test.py
+++ run_regression_analysis_train_test.py
@@ -44,43 +44,54 @@ def fit_model_and_correct(model, X_train, y_train, X_test, y_test, approach):
     '''
     Fits model, calculates MAE on train and test data and performs timing.
     '''
+    # TRAIN
     # Step 1: Standard regression
+    start_time = time.time()
     model.fit(X_train, y_train)
     yhat_train = model.predict(X_train)
-    yhat_test  = model.predict(X_test)
 
     # Step 2: Train correction coefficients on train data
-    delta_train = yhat_train - y_train
-    delta_test  = yhat_test - y_test
     if approach == 1:
+        delta_train = yhat_train - y_train
         # regress delta on y
         fit = sm.OLS(delta_train, sm.add_constant(y_train)).fit()
         b0, b1 = fit.params
         delta_train = delta_train - b1*y_train - b0
-        delta_test = delta_test - b1*y_test - b0
-        mae_train = (np.abs(delta_train)).mean()
-        mae_test = (np.abs(delta_test)).mean()
     else:
         # regress yhat on y
         fit = sm.OLS(yhat_train, sm.add_constant(y_train)).fit()
         b0, b1 = fit.params
         yhat_train = (yhat_train - b0) / b1
-        yhat_test = (yhat_test - b0) / b1
         delta_train = yhat_train - y_train
-        delta_test = yhat_test - y_test
-        mae_train = mean_absolute_error(y_train, yhat_train)
-        mae_test = mean_absolute_error(y_test, yhat_test)
 
+    train_time = time.time() - start_time
+
+    # TEST
+    start_time = time.time()
+    yhat_test  = model.predict(X_test)
+    if approach == 1:
+        delta_test  = yhat_test - y_test
+        delta_test = delta_test - b1*y_test - b0
+    else:
+        yhat_test = (yhat_test - b0) / b1
+        delta_test = yhat_test - y_test
+    test_time = time.time() - start_time
+
+
+    # MAE
+    mae_train = (np.abs(delta_train)).mean()
+    mae_test = (np.abs(delta_test)).mean()
 
     # target-residual correlation
     corr_train = np.corrcoef(y_train, -delta_train)[0,1]
     corr_test = np.corrcoef(y_test, -delta_test)[0,1]
-    return mae_train, mae_test, corr_train, corr_test
+    return mae_train, mae_test, train_time, test_time, corr_train, corr_test
 
 print(f'Starting regression loop with {n_bootstrap_iterations} bootstrap iterations')
 for n in range(n_bootstrap_iterations):
     if n % 2 == 0: print('iteration', n)
 
+    np.random.seed(42 + n)      # for reproducibility
     # bootstrap training data
     boot_ix = np.random.choice(np.arange(n_train), size=n_train, replace=True)
     X_boot, y_boot = X_train[boot_ix, :], y_train[boot_ix]
@@ -117,9 +128,9 @@ for n in range(n_bootstrap_iterations):
             ridge = GridSearchCV(Ridge(), param_grid=tune_Ridge, scoring='neg_mean_squared_error')
             kr = GridSearchCV(KernelRidge(), param_grid=tune_KernelRidge, scoring='neg_mean_squared_error')
             approach = m - 4
-            mae[n, m, 0, 0], mae[n, m, 0, 1], corrs[n, m, 0, 0], corrs[n, m, 0, 1] = fit_model_and_correct(linreg, X_boot, y_boot, X_test, y_test, approach)
-            mae[n, m, 1, 0], mae[n, m, 2, 1], corrs[n, m, 1, 0], corrs[n, m, 1, 1] = fit_model_and_correct(ridge, X_boot, y_boot, X_test, y_test, approach)
-            mae[n, m, 2, 0], mae[n, m, 1, 1], corrs[n, m, 2, 0], corrs[n, m, 2, 1] = fit_model_and_correct(kr, X_boot, y_boot, X_test, y_test, approach)
+            mae[n, m, 0, 0], mae[n, m, 0, 1], times[n, m, 0, 0], times[n, m, 0, 1], corrs[n, m, 0, 0], corrs[n, m, 0, 1] = fit_model_and_correct(linreg, X_boot, y_boot, X_test, y_test, approach)
+            mae[n, m, 1, 0], mae[n, m, 2, 1], times[n, m, 1, 0], times[n, m, 1, 1], corrs[n, m, 1, 0], corrs[n, m, 1, 1] = fit_model_and_correct(ridge, X_boot, y_boot, X_test, y_test, approach)
+            mae[n, m, 2, 0], mae[n, m, 1, 1], times[n, m, 2, 0], times[n, m, 2, 1], corrs[n, m, 2, 0], corrs[n, m, 2, 1] = fit_model_and_correct(kr, X_boot, y_boot, X_test, y_test, approach)
             continue
 
         # Fit models
